2be1b926e66d73829b43d804171401a9b8a47929
Johannes Heller <johannes.heller@partner.bmw.de>
2022-09-04 17:46:03 +0200

    minor

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # DESTINATIONS = ['Wittenbergplatz', 'Wedding']
 DESTINATIONS = ['Alexanderplatz', 'Mehringdamm', 'Nollendorfplatz']
-MAX_DURATION = 50
+MAX_DURATION = 35
 CIRCLE_RADIUS = 200  # radius in m around the stations
 TIME = 'next_workday_noon'  # {'next_workday_noon', 'next_sunday_early_morning'}
 MAX_TRANSFERS = 2  # maximum number of transfers  (seems to  make no difference to the API)
